e7831f4ea4dba68489e0463ec3aedc484aa7ef4c
nionis <stivennoni77@gmail.com>
2021-03-04 14:22:19 +0200

    dynamically update on chain or address change

--- wrapper/src/pages/index.js
+++ wrapper/src/pages/index.js
@@ -1,10 +1,11 @@
 import {
-  Link as ChakraLink,
+  Link,
   Text,
   Code,
   List,
   ListItem,
 } from '@chakra-ui/react'
+import { ExternalLinkIcon } from '@chakra-ui/icons'
 import { useEffect, useState } from "react";
 import Web3Modal from "web3modal";
 import Web3 from 'web3';
@@ -19,21 +20,23 @@ import { DarkModeSwitch } from '../components/DarkModeSwitch'
 
 
 const Index = () => {
-  const [balance, setBalance] = useState(0);
-  const [xHOPRBalance, setxHOPRBalance] = useState(0);
-  const [wxHOPRBalance, setwxHOPRBalance] = useState(0); 
-
   const [web3Modal, setWeb3Modal] = useState();
   const [isLoading, setLoading] = useState(false);
   const [provider, setProvider] = useState();
   const [chainId, setChainId] = useState();
-  const [wallet, setWallet] = useState();
   const [address, setAddress] = useState();
+  const [txHash, setTxHash] = useState();
+  const [xHOPRBalance, setxHOPRBalance] = useState(0);
+  const [wxHOPRBalance, setwxHOPRBalance] = useState(0); 
 
   const XDAI_CHAIN_ID = 100;
   const xHOPR_TOKEN_ADDRESS = "0xD057604A14982FE8D88c5fC25Aac3267eA142a08";
   const wrapper_CONTRACT_ADDRESS = "0x097707143e01318734535676cfe2e5cF8b656ae8";
   const wxHOPR_TOKEN_ADDRESS = "0xD4fdec44DB9D44B8f2b6d529620f9C0C7066A2c1";
+  const tokenAddresses = [xHOPR_TOKEN_ADDRESS, wxHOPR_TOKEN_ADDRESS]
+  const ERC20_balanceOf_ABI = [
+    "function balanceOf(address) view returns (uint)",
+  ]
 
   const transferAndCallToken = async (provider, amount) => {
     const abi = ['function transferAndCall(address, uint256, bytes)'];
@@ -55,54 +58,80 @@ const Index = () => {
     return tokenContract.transfer(wrapper_CONTRACT_ADDRESS, ethers.utils.parseEther(amount));
   };
 
+  // trigger on initial load
   useEffect(() => {
     const loadModal = () => {
       const _web3Modal = new Web3Modal({
         cacheProvider: true,
-        providerOptions: {}
+        providerOptions: {},
       });
-      setWeb3Modal(_web3Modal)
-    }
-    loadModal()
-  }, [])
+      setWeb3Modal(_web3Modal);
+    };
+    loadModal();
+  }, []);
 
-  //@TODO: Refactor to a proper Web3Context
-  const handleConnectWeb3 = async() => {
-    setLoading(true)
-    const tokenAddresses = [xHOPR_TOKEN_ADDRESS, wxHOPR_TOKEN_ADDRESS]
+  // trigger once provider is set
+  useEffect(() => {
+    const updateProviderData = async () => {
+      setLoading(true);
+
+      const network = await provider.getNetwork();
+      const address = await provider.getSigner().getAddress();
+
+      setChainId(network.chainId);
+      setAddress(address);
+      setLoading(false);
+    };
+
+    if (provider) updateProviderData();
+  }, [provider]);
+
+  // trigger once chain or address changes
+  useEffect(() => {
+    const updateUserData = async () => {
+      setLoading(true);
+
+      const tokenContracts = tokenAddresses.map(tokenAddress => new ethers.Contract(tokenAddress, ERC20_balanceOf_ABI, provider));
+      const [xHOPRBalance, wxHOPRBalance] = await Promise.all(tokenContracts.map(async (contract) => {
+        try {
+          return await contract.balanceOf(address)
+        } catch {
+          return 0
+        }
+      }))
+
+      setxHOPRBalance(ethers.utils.formatEther(xHOPRBalance))
+      setwxHOPRBalance(ethers.utils.formatEther(wxHOPRBalance))
+      setLoading(false);
+    };
+
+    if (chainId && address) updateUserData();
+  }, [chainId, address, txHash]);
+
+  const handleConnectWeb3 = async () => {
+    setLoading(true);
 
     const modalProvider = await web3Modal.connect();
     const web3Provider = new Web3(modalProvider);
     const provider = new ethers.providers.Web3Provider(
-      web3Provider.currentProvider,
+      web3Provider.currentProvider
     );
 
-    const wallet = provider.getSigner();
-    const address = await wallet.getAddress();
-    const balance = await provider.getBalance(address);
-    const network = await provider.getNetwork();
-
-    const tokenABI = [
-      "function balanceOf(address) view returns (uint)",
-    ]
-
-    const tokenContracts = tokenAddresses.map(tokenAddress => new ethers.Contract(tokenAddress, tokenABI, provider));
-    const [xHOPRBalance, wxHOPRBalance] = await Promise.all(tokenContracts.map(async (contract) => {
-      try {
-        return await contract.balanceOf(address)
-      } catch {
-        return 0
-      }
-    }))
+    modalProvider.on("accountsChanged", (accounts) => setAddress(accounts[0]));
+    modalProvider.on("chainChanged", (chainId) =>
+      setChainId(parseInt(chainId, 16))
+    );
 
     setProvider(provider);
-    setChainId(network.chainId);
-    setWallet(wallet);
-    setAddress(address);
-    setBalance(ethers.utils.formatEther(balance))
-    setxHOPRBalance(ethers.utils.formatEther(xHOPRBalance))
-    setwxHOPRBalance(ethers.utils.formatEther(wxHOPRBalance))
-    console.log(network, balance, wallet, provider);
+    setLoading(false);
+  };
+
+  const HandleSwap = (swap) => async () => {
+    setLoading(true)
+
+    const receipt = await swap()
+    setTxHash(receipt.hash)
+
     setLoading(false)
   }
 
@@ -115,15 +144,15 @@ const Index = () => {
         Utility to wrap <Code>(xHOPR -> wxHOPR)</Code> and unwrap <Code>(wxHOPR -> xHOPR)</Code> xHOPR tokens.
       </Text>
       {
-        !provider ? null : wrongNetwork ? (
+        !address ? null : wrongNetwork ? (
           <Text textAlign="center">Please switch to the xDAI chain.</Text>
         ) : (
           <List spacing={3} my={0}>
             <ListItem>
-              <TokenInput symbol="xHOPR" address={address} value={xHOPRBalance} setValue={setxHOPRBalance} handleSwap={() => transferAndCallToken(provider, xHOPRBalance)} />
+              <TokenInput symbol="xHOPR" address={address} value={xHOPRBalance} setValue={setxHOPRBalance} handleSwap={HandleSwap(() => transferAndCallToken(provider, xHOPRBalance))} />
             </ListItem>
             <ListItem>
-              <TokenInput symbol="wxHOPR" address={address} value={wxHOPRBalance} setValue={setwxHOPRBalance} handleSwap={() => transferToken(provider, wxHOPRBalance)} />
+              <TokenInput symbol="wxHOPR" address={address} value={wxHOPRBalance} setValue={setwxHOPRBalance} handleSwap={HandleSwap(() => transferToken(provider, wxHOPRBalance))} />
             </ListItem>
           </List>
         )
@@ -133,6 +162,18 @@ const Index = () => {
         onClick={handleConnectWeb3}
         isLoading={isLoading}
       />
+      {txHash ? (
+        <Text>
+          Swapping is under way! Check out the transaction progress{" "}
+          <Link
+            href={`https://blockscout.com/poa/xdai/tx/${txHash}`}
+            isExternal
+          >
+            here <ExternalLinkIcon mx="2px" />
+          </Link>
+          .
+        </Text>
+      ) : null}
     </Main>
     <DarkModeSwitch />
   </Container>);
